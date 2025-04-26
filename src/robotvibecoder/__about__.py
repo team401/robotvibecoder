17a58b1e8bfd551a12e9e00cd75622bd7628ef4e
aidnem <99768676+aidnem@users.noreply.github.com>
2025-04-25 21:27:13 -0400

    (#28) Add versioningit and replace aidnem with team 401

--- src/robotvibecoder/__about__.py
+++ src/robotvibecoder/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025-present aidnem <99768676+aidnem@users.noreply.github.com>
+# SPDX-FileCopyrightText: 2025-present team401
 #
 # SPDX-License-Identifier: GPL-3.0-only
 __version__ = "0.0.2"
